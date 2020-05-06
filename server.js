e25037aa4c5a4f2a41a5f81fb2255954c1856e5b
karlwitek <karl.witek20@gmail.com>
2020-05-05 17:19:39 -0700

    munge function

--- server.js
+++ server.js
@@ -6,14 +6,22 @@ const express = require('express');
 
 const cors = require('cors');
 
-const data = require('./data/weather.json');
+const weatherData = require('./data/weather.json');
+const geoData = require('./data/geo.json');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
-function mungeWeather(data) {
+function mungeWeather(weatherData) {
+    return weatherData.map(item => {
+        const object = {};
+        object.forecast = item.description;
+        object.time = item.datetime;
+        return object;
+    })
+
 
 
 }
@@ -21,7 +29,7 @@ function mungeWeather(data) {
 app.get('/location', (req, res) => {
 
     try {
-        const mungedResponse = mungeWeather(data);
+        const mungedResponse = mungeWeather(weatherData);
         res.json(mungedResponse);        
     }  catch(e) {
         console.error(e);
@@ -32,3 +40,27 @@ app.get('/location', (req, res) => {
         });
     }
 })
+
+app.get('weather', (req, res) => {
+    res.json([
+        {
+            forecast: 'Partly cloudy until afternoon.',
+            time: 'Mon Jan 01 2001',
+        },
+        {
+            forecast: 'Mostly cloudy in the morning.',
+            time: 'Tue Jan 02 2001',
+        },
+    ]);
+
+
+});
+
+app.get('*', (req, res) => {
+    res.status(404).json({
+    error: 'no such route!',
+});
+});
+
+app.listen(PORT, () => { console.log(`listening on port ${PORT}`);});
+
